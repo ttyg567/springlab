44f9e9d9f9117d2348c0a76127275a96c5e73e81
ttyg567@gmail.com <ttyg567@gmail.com>
2023-04-18 16:05:52 +0900

    0418_2

---
+++ day024_AOP/src/main/java/com/kbstar/service/UserService.java
@@ -0,0 +1,51 @@
+package com.kbstar.service;
+
+import com.kbstar.dto.UserDTO;
+import com.kbstar.frame.MyDao;
+import com.kbstar.frame.MyService;
+import com.kbstar.dao.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+@Service("uservice")
+public class UserService implements MyService<String, UserDTO> {
+
+    // 방식 1
+    //    @Autowired
+    //    @Qualifier("udao")
+    //   MyDao<String, UserDTO> dao;
+
+    // 방식 2
+    @Autowired
+    UserDAO dao;
+
+    @Override
+    public void register(UserDTO userDTO) {
+        dao.insert(userDTO);
+        System.out.println("Send Mail");
+    }
+
+    @Override
+    public void remove(String s) {
+        dao.delete(s);
+    }
+
+    @Override
+    public void modify(UserDTO userDTO) {
+        dao.update(userDTO);
+        System.out.println("Send Mail ..");
+    }
+
+    @Override
+    public UserDTO get(String s) {
+        return dao.select(s);
+    }
+
+    @Override
+    public List<UserDTO> get() {
+        return dao.select();
+    }
+}
