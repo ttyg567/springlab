3de8cc694c0ce0f2e327cc87fbe8353f8bb4d7ce
ttyg567@gmail.com <ttyg567@gmail.com>
2023-04-19 13:43:43 +0900

    0419

---
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.kbstar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    // http://127.0.0.1/
+    @RequestMapping("/")
+    public String main(){
+        return "index";
+    }
+
+    @RequestMapping("/nex")
+    public String next(){
+        return "next";
+    }
+}
---
+++ src/main/webapp/views/next.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ttyg567
+  Date: 2023-04-19
+  Time: 오후 1:35
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Next Page</title>
+</head>
+<body>
+    <h1> Next Page</h1>
+</body>
+</html>
