288edb78afbbfd651ecca0b561d23aeb0e475687
ttyg567@gmail.com <ttyg567@gmail.com>
2023-04-26 13:13:52 +0900

    0426

---
+++ src/main/java/com/kbstar/Web09Application.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web09Application.class, args);
+    }
+
+}
