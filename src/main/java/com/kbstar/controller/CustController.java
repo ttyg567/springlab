e7de9e03c9576525ad42304bb1ba4ba0f9979e29
ttyg567@gmail.com <ttyg567@gmail.com>
2023-04-28 09:13:14 +0900

    0428

---
+++ src/main/java/com/kbstar/controller/CustController.java
@@ -0,0 +1,62 @@
+package com.kbstar.controller;
+
+import com.kbstar.dto.Cust;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Slf4j
+@Controller
+@RequestMapping("/cust")
+public class CustController {
+    //Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+    String dir = "cust/";
+    // 위에 /cust 이므로, "" 이렇게만 쳤을 때 127.0.0.1/cust 가 된다
+    @RequestMapping("")
+    public String main(Model model) {
+        model.addAttribute("left", dir+"left");
+        model.addAttribute("center", dir+"center");
+        //logger.info("-------------------------------------");
+        Random r = new Random();
+        int inx = r.nextInt(1000)+1;
+        log.info(inx+"");
+        return "index";
+    }
+
+    @RequestMapping("/add")
+    public String add(Model model) {
+        model.addAttribute("left", dir+"left");
+        model.addAttribute("center", dir+"add");
+        return "index";
+    }
+
+    @RequestMapping("/get")
+    public String get(Model model, String id) {
+        Cust cust = new Cust(id, "xxx", "james");
+        model.addAttribute("gcust", cust);
+        model.addAttribute("left", dir+"left");
+        model.addAttribute("center", dir+"get");
+        return "index";
+    }
+
+    @RequestMapping("/all")
+    public String all(Model model) {
+        List<Cust> list = new ArrayList<>();
+        list.add(new Cust("id01", "pwd01", "james1"));
+        list.add(new Cust("id02", "pwd02", "james2"));
+        list.add(new Cust("id03", "pwd03", "james3"));
+        list.add(new Cust("id04", "pwd04", "james4"));
+        list.add(new Cust("id05", "pwd05", "james5"));
+
+        model.addAttribute("clist", list);
+        model.addAttribute("left", dir+"left");
+        model.addAttribute("center", dir+"all");
+        return "index";
+    }
+
+}
