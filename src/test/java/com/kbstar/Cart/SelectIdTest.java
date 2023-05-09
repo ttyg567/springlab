8ef37d779ccaaca1074fe62a357415f6acea66e6
ttyg567@gmail.com <ttyg567@gmail.com>
2023-05-09 09:06:42 +0900

    0509

---
+++ src/test/java/com/kbstar/Cart/SelectIdTest.java
@@ -0,0 +1,31 @@
+package com.kbstar.Cart;
+
+import com.kbstar.dto.Cart;
+import com.kbstar.service.CartService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+@Slf4j
+class SelectIdTest {
+    @Autowired
+    CartService service;
+    @Test
+    void contextLoads() {
+        List<Cart> list = null;
+        try {
+            list = service.getmycart("idid");
+//            for (Cust obj:list){
+//                log.info(obj.toString());
+//            }
+        } catch (Exception e) {
+            log.info("시스템 장애입니다.==============================");
+                e.printStackTrace();
+            }
+    }
+
+}
